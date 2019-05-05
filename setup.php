404efc61efccfebb43ecff59b0f1a853c8dc4f5c
blestab <blestab@users.noreply.github.com>
2019-05-05 18:17:10 +0200

    feat: remove Guest migration - fails in Heroku

--- setup.php
+++ setup.php
@@ -7,7 +7,8 @@ $app = new App(true);
 //Migrate necessary tables
 $app->add('\atk4\schema\MigratorConsole')
     ->migrateModels([
-        new Guest($app->db), 
+      //Tried to add Guest to a migration but kept geting an error in heroku, locally it seems fine
+      //  new Guest($app->db),
         new \atk4\login\Model\User($app->db)
     ]);
 
